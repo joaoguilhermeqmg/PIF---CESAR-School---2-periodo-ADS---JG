a6a7c70017ad106db81413b12e7a28ec92d5ea09
joaoguilhermeqmg <joaoguilhermeqmg@gmail.com>
2026-08-24 20:28:13 -0300

    Update q04.c
    Corrected syntax errors and updated main function.

--- Lista-cqp1/q04.c
+++ Lista-cqp1/q04.c
@@ -1,1 +1,19 @@
+/*ERROS DO CÓDIGO:
+Colocou ";" na linha da biblioteca stdlib.h 
+O "main" ta escrito com letra maiúscula
+Colocou parenteses em vez de chaves na função "main"
+A linha (cout << endl;) não existe na linguagem C
+*/
 
+
+
+//CÓDIGO CORRETO:
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    printf("Existem %d semanas no ano\n", 52);
+
+    system("PAUSE");
+
+    return 0;
+}
