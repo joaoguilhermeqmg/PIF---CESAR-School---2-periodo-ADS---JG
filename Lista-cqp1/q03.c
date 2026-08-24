f6edd0584a73b62b3904813a5e44d943b849731b
joaoguilhermeqmg <joaoguilhermeqmg@gmail.com>
2026-08-24 20:27:50 -0300

    Update q03.c

--- Lista-cqp1/q03.c
+++ Lista-cqp1/q03.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+int main(){
 
+    //Declarando as variáveis
+    int num1 = 10;
+    int num2 = 20;
+
+    //Fazendo a soma dos números
+    int soma = num1 + num2;
+
+    //Exibindo o resultado
+    printf("A soma de %d e %d é %d", num1, num2, soma);
+
+    return 0;
+}
