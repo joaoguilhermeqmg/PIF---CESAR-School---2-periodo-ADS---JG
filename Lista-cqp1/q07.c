06afffff5f6bc0cf83e3f5ef6626f3513a59a364
joaoguilhermeqmg <joaoguilhermeqmg@gmail.com>
2026-08-24 20:30:49 -0300

    Update q07.c
    Added comments explaining the functionality of the program.

--- Lista-cqp1/q07.c
+++ Lista-cqp1/q07.c
@@ -1,1 +1,8 @@
-
+/* 
+a)O programa mostra a mensagem "Bom dia! Shirley." com uma tabulação antes da frase e uma quebra de linha antes da tabulação.
+b)O programa mostra a mensagem "Você já tomou café?" com uma quebra de linha no final da frase.
+c)O programa mostra a mensagem "A solução não existe! Não insista." com duas quebras de linha no início da frase e uma quebra de linha depois de "A solução não existe!".
+d)O programa mostra a mensagem "Duas    linhas    de    saída
+ou    uma?" com tabulações entre as palavras "Duas" e "linhas", "linhas" e "de", "de" e "saída", "ou" e "uma?", e uma quebra de linha antes da palavra "ou".
+e)O programa mostra a mensagem "um", "dois" e "três" em três linhas diferentes, cada uma com uma quebra de linha no final.
+*/
