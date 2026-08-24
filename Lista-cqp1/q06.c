2e3b8c5358d1ecbee2ba6de4f53469c4fa2e8c2a
joaoguilhermeqmg <joaoguilhermeqmg@gmail.com>
2026-08-24 20:30:25 -0300

    Update q06.c
    Added necessary includes and corrected main function declaration.

--- Lista-cqp1/q06.c
+++ Lista-cqp1/q06.c
@@ -1,1 +1,14 @@
+/*
+Faltou incluir as bibliotecas <stdio.h> e <stdlib.h>
+Não declarou a variável main como (int) e não colocou o return 0 no final
+Na linha das variáveis está com ";" em vez de "," entre as variáveis e a linha do código deve terminar com ";" e não ":"
 
+*/
+#include <stdio.h>
+int main(){
+    int a=1, b=2, c=3;
+    printf("Os números são: %d, %d e %d\n", a, b, c);
+    system("pause");
+
+    return 0;
+}
