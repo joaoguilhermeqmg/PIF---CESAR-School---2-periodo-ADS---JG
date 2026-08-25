3afa76d061e436e435c610b43ff96fc6388417c2
joaoguilhermeqmg <joaoguilhermeqmg@gmail.com>
2026-08-24 21:45:40 -0300

    Update q10.c

--- Lista-cqp1/q10.c
+++ Lista-cqp1/q10.c
@@ -1,1 +1,2 @@
-
+/*Alternativa correta: Letra B
+A linguagem C é case sensitive, isso significa que letras maiúsculas e minúsculas são tratadas como caracteres diferentes, ou seja, 'peso','Peso' e 'PESO' são variáveis diferentes.*/
