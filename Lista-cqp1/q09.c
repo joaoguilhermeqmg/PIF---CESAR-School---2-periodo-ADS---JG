714e259c7d22043d8e84cc2ce1e9f407c0ba09e9
joaoguilhermeqmg <joaoguilhermeqmg@gmail.com>
2026-08-24 21:07:47 -0300

    Update q09.c
    Add a simple C program that prints a message with formatting.

--- Lista-cqp1/q09.c
+++ Lista-cqp1/q09.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(){
+printf("%c%c%cPrimeiro programa", '\n', '\t', '\"');
+printf("%c", "\"");
+system("PAUSE");
+return 0;
+}
+
+/*O compilador interpreta os caracteres simples da seguinte forma:
+- '\n' é interpretado como um caractere de nova linha
+- '\t' é interpretado como um caractere de tabulação
+- '\"' é interpretado como um caractere de aspa dupla */
