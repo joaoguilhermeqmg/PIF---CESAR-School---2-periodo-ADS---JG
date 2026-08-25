c2de6ed30e64887cd991be9d05872fc5d32e6939
joaoguilhermeqmg <joaoguilhermeqmg@gmail.com>
2026-08-24 21:07:14 -0300

    Update q08.c

--- Lista-cqp1/q08.c
+++ Lista-cqp1/q08.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+printf("\n\t\"Primeiro programa\"");
+system("PAUSE");
+return 0;
+}
 
+/* O programa mostra a mensagem "Primeiro programa" com uma tabulação antes da frase e uma quebra de linha antes da tabulação, depois ele pausa o programa e pede para apertar uma tecla para continuar. */
