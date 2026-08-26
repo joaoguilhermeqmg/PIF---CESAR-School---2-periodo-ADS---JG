9012667bc5c0f837f34ae77ae5cb8a9333d0f07c
joaoguilhermeqmg <joaoguilhermeqmg@gmail.com>
2026-08-26 00:42:12 -0300

    Create q26.c

---
+++ Lista-cqp1/q26.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    printf("      +\n");
+    printf("      /\\\n");
+    printf("     / o\\\n");
+    printf("    /o   \\\n");
+    printf("   /   o  \\\n");
+    printf("  / o    o \\\n");
+    printf(" /__________\\\n");
+    printf("     |  |\n");
+    printf("     ==== \n");
+    return 0;
+}
