327bed445d3532f484d43d91fa167b1af01497bb
joaoguilhermeqmg <joaoguilhermeqmg@gmail.com>
2026-08-26 00:35:18 -0300

    Add q18.c to display item prices

---
+++ Lista-cqp1/q18.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    float lapis = 4.88;
+    float borrachas = 234.54;
+    float canetas = 42.04;
+    float cadernos = 8.00;
+    float fitas = 13.05;
+    printf("%s%12.2f\n","Lapis",lapis);
+    printf("%s%12.2f\n","Borrachas",borrachas);
+    printf("%s%12.2f\n","Canetas",canetas);
+    printf("%s%12.2f\n","Cadernos",cadernos);
+    printf("%s%12.2f\n","Fitas",fitas);
+    return 0;
+}
