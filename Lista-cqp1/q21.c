e0454b1a9c62cfc4fdcb80676603240b885f606d
joaoguilhermeqmg <joaoguilhermeqmg@gmail.com>
2026-08-26 00:37:48 -0300

    Add three C programs for output formatting examples
    This file contains three separate C programs demonstrating different styles of output formatting.

---
+++ Lista-cqp1/q21.c
@@ -0,0 +1,29 @@
+//Primeira parte
+#include <stdio.h>
+
+int main(){
+    printf("Treinamento em programação.\nLinguagem C.\n");
+    return 0;
+}
+
+
+//Segunda parte
+#include <stdio.h>
+
+int main(){
+    printf("Treinamento em programação.\n");
+    printf("Linguagem C.");
+    return 0;
+}
+
+
+//Terceira parte
+#include <stdio.h>
+
+int main(){
+    printf("------------------------------\n");
+    printf("| Treinamento em programação.|\n");
+    printf("| Linguagem C.               |\n");
+    printf("------------------------------\n");
+    return 0;
+}
