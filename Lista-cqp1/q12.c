ee2f6d95395c7cb00b4bd098f569bdccb1cac519
joaoguilhermeqmg <joaoguilhermeqmg@gmail.com>
2026-08-26 00:32:52 -0300

    Update q12.c
    Added comments regarding variable declarations and their correctness.

--- Lista-cqp1/q12.c
+++ Lista-cqp1/q12.c
@@ -1,1 +1,10 @@
-
+/*
+a) int a; [  correto  ] [  ]
+b) float b; [  correto  ] [  ]
+c) double float c; [  incorreto  ] [ float e double não podem ser usados juntos ]
+d) unsigned char d; [  correto  ] [  ]
+e) unsigned e; [  correto  ] [  ]
+f) long float f; [  incorreto  ] [ o modficador long não pode ser usado com float ]
+g) long g; [  correto  ] [  ]
+h) long double h; [  correto  ] [  ]
+*/
